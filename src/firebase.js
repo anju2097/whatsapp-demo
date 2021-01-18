7a34d353ff055f478384774707023ad52f6387d8
anju2097 <33646195+anju2097@users.noreply.github.com>
2021-01-18 18:30:13 +0530

    Create firebase database and update this file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQ3H3MTpt6ifc8C-xRjrjAk0INjoakfeU",
-    authDomain: "whats-app-clone-a991f.firebaseapp.com",
-    projectId: "whats-app-clone-a991f",
-    storageBucket: "whats-app-clone-a991f.appspot.com",
-    messagingSenderId: "239234370072",
-    appId: "1:239234370072:web:24b6e7c0608f02700c516d",
-    measurementId: "G-5Q2VSEYJLY"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
